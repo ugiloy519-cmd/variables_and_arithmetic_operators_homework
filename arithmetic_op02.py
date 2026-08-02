517ceba9292303a25e46455ebea0058611ad656c
Ugiloy <ugiloy519@gmail.com>
2026-08-02 20:11:50 +0500

    solved

--- arithmetic_op02.py
+++ arithmetic_op02.py
@@ -1,5 +1,8 @@
 # Create a variable "a"  and assign it the value of the int 3.
-
+a=3
+b=4
+answer=a+b
+print(answer)
 # Create a variable "b"  and assign it the value of the int 4.
 
 # Create a variable called 'answer' and assign it the sum of a and b.
