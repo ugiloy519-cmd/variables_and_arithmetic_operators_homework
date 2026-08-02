6354f69ef3fa0c2147f397be00a536c8a0a13f5c
Ugiloy <ugiloy519@gmail.com>
2026-08-02 20:13:40 +0500

    solved

--- arithmetic_op05.py
+++ arithmetic_op05.py
@@ -1,5 +1,8 @@
 # Create a variable "a"  and assign it the value of the int 72.
-
+a=72
+b=9
+answer=a/b
+print(answer)
 # Create a variable "b"  and assign it the value of the int 9.
 
 # Create a variable called "answer" and assign it the division of a by b.
