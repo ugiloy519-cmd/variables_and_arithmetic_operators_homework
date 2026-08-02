9a36300fec9583b38de04a1a8df7e8365f24a6af
Ugiloy <ugiloy519@gmail.com>
2026-08-02 20:12:39 +0500

    solved

--- arithmetic_op03.py
+++ arithmetic_op03.py
@@ -1,5 +1,8 @@
 # Create a variable "a"  and assign it the value of the int 6.
-
+a=6
+b=2
+answer=a-b
+print(answer)
 # Create a variable "b"  and assign it the value of the int 2.
 
 # Create a variable called 'answer' and assign it the difference of a and b.
