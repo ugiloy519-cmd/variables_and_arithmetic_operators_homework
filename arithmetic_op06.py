4a713d0b7e0810d786a9bdf853c5039a2006010c
Ugiloy <ugiloy519@gmail.com>
2026-08-02 20:14:04 +0500

    solved

--- arithmetic_op06.py
+++ arithmetic_op06.py
@@ -1,5 +1,10 @@
 # Create a variable "a"  and assign it the value of the int 7.
-
+a=7
+b=3
+c=5
+answer=a+b+c
+print(answer)
+# Create a variabl
 # Create a variable "b"  and assign it the value of the int 3.
 
 # Create a variable "c"  and assign it the value of the int 5.
